b31fdaf796c80e73e62eaf573033845d22f8f646
mrusso <mario.russo@rumbo.com>
2017-04-17 19:31:43 +0200

    added test to certify that we can manage more thant not zero rolls

--- bowling/src/test/java/com/rmarioo/playground/bowling/BowlingTest.java
+++ bowling/src/test/java/com/rmarioo/playground/bowling/BowlingTest.java
@@ -25,6 +25,15 @@ public class BowlingTest
     assertThat(bowling.score(),is(2));
   }
 
+  @Test
+  public void moreNotZeroRolls() throws Exception
+  {
+    bowling.roll(2);
+    bowling.roll(3);
+    rollMany(0, 18);
+    assertThat(bowling.score(),is(5));
+  }
+
   private void rollMany(int score, int howMany)
   {
     for (int i = 0; i < howMany; i++)
